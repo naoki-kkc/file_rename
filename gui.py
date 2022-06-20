a78cd49d51bba61d80ddb83e6cb508b38751150b
naoki-kkc <naoki2kuchi@gmail.com>
2022-06-21 00:25:48 +0900

    add buttons, implements more drop process

--- gui.py
+++ gui.py
@@ -1,25 +1,43 @@
 import tkinter as tk
 import tkinterdnd2 as tkdnd2
+import os
 
 # labelにドロップされた時に呼ばれる関数
 def after_drop(event):
     # print(event.data)
-    label_text.set(event.data)
+    path_str = event.data
+    
+    # パスの種類で実行ボタンの有効/無効を制御
+    if os.path.isdir(path_str):
+        label_text.set(path_str)
+        exec_button['state'] = tk.NORMAL
+    else:
+        label_text.set('ドロップされたパスはディレクトリではありません')
+        exec_button['state'] = tk.DISABLED
 
 # root = tk.Tk()
 root = tkdnd2.Tk() # ドラッグ&ドロップを実現するため、tkinterdnd2でメインウインドウを作成
 
 root.title('file_rename') # タイトル
-root.geometry('300x300')  # 画面サイズ
+root.geometry('600x150')  # 画面サイズ
 
 # ドラッグドロップ用ラベルを作成
 root.drop_target_register(tkdnd2.DND_FILES) # ドロップ対象はファイル
+root.dnd_bind('<<Drop>>', after_drop)       # ドロップされたイベントを検知
+
+# ラベル部分　
 label_text = tk.StringVar(root)
-label_text.set('ファイルをドロップするとファイルパスを表示します')
+label_text.set('ウインドウ内にファイルをドロップするとファイルパスを表示します')
 label = tk.Label(root, textvariable=label_text)
-label.drop_target_register(tkdnd2.DND_FILES)
-label.dnd_bind('<<Drop>>', after_drop)
+
+# 実行ボタン/終了ボタン
+exec_button  = tk.Button(root, text='実行', width=30, state=tk.DISABLED)
+close_button = tk.Button(root, text='終了', width=30, state=tk.NORMAL)
+
+# レイアウト
 label.grid(row=0, column=0, padx=10)
+exec_button.grid(row=1, column=0, padx=20)
+close_button.grid(row=1, column=1, padx=10)
 
 # 画面表示
 root.mainloop()
